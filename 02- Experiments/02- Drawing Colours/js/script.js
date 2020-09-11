5d5053e8994e7adc48ddcf3d521a7868243bf07d
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-11 13:58:53 -0400

    02-Drawing Colours exercise

---
+++ 02- Experiments/02- Drawing Colours/js/script.js
@@ -0,0 +1,22 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+  background(500,500)
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
