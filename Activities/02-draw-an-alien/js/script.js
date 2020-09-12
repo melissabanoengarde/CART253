e7c46c80a75c65784a4f2355de5cd162e9ff7564
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-12 13:43:03 -0400

    Starting the "02- Draw an aline" activity

--- Activities/02-draw-an-alien/js/script.js
+++ Activities/02-draw-an-alien/js/script.js
@@ -1,14 +1,23 @@
 /**************************************************
 Template p5 project
-Pippin Barr
+Melissa Banoen-Garde
 
-Here is a description of this template p5 project.
+Activity 02: Drawing an alien
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+
+//ENVIRONMENT
+  createCanvas(640,480);
+  background(255,204,204);
+
+//ALIEN
+  noStroke();
+  fill();
+  ellipse();
 
 }
 
