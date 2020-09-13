06f6be2bd6b1bb91a1868e339c4ba501be0fea2a
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-13 02:04:17 -0400

    Added cheekbones and tweeked existing colours

--- Activities/02-draw-an-alien/js/script.js
+++ Activities/02-draw-an-alien/js/script.js
@@ -12,14 +12,14 @@ function setup() {
 
 //ENVIRONMENT
   createCanvas(640,480);
-  background(209, 130, 51);
+  background(186, 122, 59);
 
 //ALIEN
 //Body
   noStroke();
 
   //core
-  fill(98, 128, 157);
+  fill(79, 104, 125);
   rectMode(CENTER);
   rect(320,400,170,180,15,15,0,0)
 
@@ -30,6 +30,7 @@ function setup() {
   //ellipse(343,460,30,15);
 
 //Head
+ellipseMode(CENTER);
   fill(88, 116, 141);
   ellipse(320,160,200,280);
   rect(320,180,230,150,50,50,90,90);
@@ -42,6 +43,12 @@ function setup() {
   rect(320,300,110,50,0,0,0,0);
   rect(320,330,80,20,0,0,0,0);
   rect(320,350,38,20,0,0,0,0);
+
+  //cheekbones
+  fill(79, 104, 125,110)
+  quad(238,240,260,320,310,370,250,240);          //quad(x1, y1, x2, y2, x3, y3, x4, y4)
+  quad(400,240,380,320,330,370,390,240);
+
 
   //eyes
   stroke(0);
