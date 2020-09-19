6fa6663ae6a489fa63e5cc42ef47beca89c35311
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-19 17:06:17 -0400

    Update script.js

--- Experiments/variables-experiments/js/script.js
+++ Experiments/variables-experiments/js/script.js
@@ -12,7 +12,7 @@ by Pippin Barr.
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500,500);
 
 
 }
@@ -22,7 +22,10 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(250,0,0);
-  rectMode(CENTER);
-  rect(width/2,height/2,100,100);
+  ellipse(250,250,400,400);
+  ellipse(250,250,300,300);
+  ellipse(250,250,200,200);
+  ellipse(250,250,100,100);
+  }
 
 }
