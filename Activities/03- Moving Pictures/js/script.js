73da061ae8a71761a021848ee515cc51940c1b66
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-20 14:41:18 -0400

    Starting "Moving Pictures" activity
    Activity 03: Moving Pictures / using variables; declaring and chcanging variables esp JavaScript objects / using map and constrain

---
+++ Activities/03- Moving Pictures/js/script.js
@@ -0,0 +1,24 @@
+/**************************************************
+Activity 03: Moving Pictures
+CART 253-B
+Melissa Banoen-Garde
+
+Programming moving pictures, referring to video
+"03-8 - Moving Pictures" posted on Moodle and
+"Activity 03: Moving Pictures" lecture notes, provided
+by Pippin Barr.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
