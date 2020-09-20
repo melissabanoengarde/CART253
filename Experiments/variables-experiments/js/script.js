d9798a8f981257e246d77b0741f37f3319908b73
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-20 11:30:35 -0400

    Experimented with changing variables
    "+=" and "-=" / colours are numbers too / all numbers should be ideally represented in variables / negative numbers / changing variables that changes variabels

--- Experiments/variables-experiments/js/script.js
+++ Experiments/variables-experiments/js/script.js
@@ -9,9 +9,11 @@ by Pippin Barr.
 **************************************************/
 
 let bgShade = 0;
-let circleX = 250;
+let circleX = 0;
 let circleY = 250;
 let circleSize = 200;       //"let" means declaring a variable to js
+let circleSpeed = 2;
+let circleAcceleration = 0.25;
 
 // setup()
 //
@@ -23,9 +25,11 @@ function setup() {
 
 // draw()
 //
-// Description of draw() goes here.
+// Variables experiment below
 function draw() {
   background(bgShade);
+  circleX -= circleSpeed;
+  circleSpeed -= circleAcceleration;
   ellipse(circleX,circleY,circleSize);
 
 
