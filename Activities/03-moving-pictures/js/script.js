af71d121a5e52255f72457c97c532f22ba308a00
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-20 18:46:54 -0400

    Completed the the A3 activity!

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -15,6 +15,8 @@ let circleL ={
   x:0,
   y:250,
   size:100,
+  growthRate:1,
+  speed:1,
   fill:255,
   alpha:200
 
@@ -24,6 +26,8 @@ let circleR ={
   x:700,
   y: 250,
   size:75,
+  sizeRatio: 0.75,
+  speed:-1,
   fill:255,
   alpha:200
 
@@ -45,7 +49,26 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  //Background
   background(bg.r, bg.g, bg.g);
-  bg.r = bg.r +1;
+  bg.r = map(circleL.size,100,width,0,255);
+
+  //Left Circle
+  circleL.x = circleL.x + circleL.speed;
+  circleL.x = constrain(circleL.x,0,width/2);
+  circleL.size = circleL.size + circleL.growthRate;
+  circleL.size = constrain(circleL.size,0,width);
+  fill(circleL.fill,circleL.alpha);
+  ellipse(circleL.x,circleL.y,circleL.size);
+
+
+  //Right Circle
+  circleR.x = circleR.x + circleR.speed;      //'add' a negative 1
+  circleR.x = constrain(circleR.x,width/2,width);
+  circleR.size = circleL.size * circleR.sizeRatio;    //cool!!
+  fill(circleR.fill,circleR.alpha);
+  ellipse(circleR.x,circleR.y,circleR.size);
+
+
 
 }
