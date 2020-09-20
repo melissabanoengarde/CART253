61b2fb2e45c706083b2bc73b4082250dcb464c77
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-19 22:55:57 -0400

    Created new "variables-experimets" folder, continuing variables exercises

--- Experiments/(LIVE PREVIEWNOT WORKING)variables-experiments/js/script.js
+++ Experiments/(LIVE PREVIEWNOT WORKING)variables-experiments/js/script.js
@@ -1,8 +1,11 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Variables Experiments
+CART 253-B
+Melissa Banoen-Garde
 
-Here is a description of this template p5 project.
+Experimenting with variables, referring to videos
+posted on Moodle and lecture notes--both provided
+by Pippin Barr.
 **************************************************/
 
 // setup()
@@ -10,17 +13,19 @@ Here is a description of this template p5 project.
 // Description of setup() goes here.
 function setup() {
   createCanvas(500,500);
-//  background(250,0,0);
+
+
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(255,0,0);
+  background(250,0,0);
   ellipse(250,250,400,400);
   ellipse(250,250,300,300);
   ellipse(250,250,200,200);
   ellipse(250,250,100,100);
+  }
 
 }
