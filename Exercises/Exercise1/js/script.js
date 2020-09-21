6124afabaeb909e8b0c9ebbd31a8c39b650705a7
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-21 18:14:14 -0400

    E1: I will now set variables for my jar and lid
    Now that have an idea of how I want the setup to appear, I will now set variables for the following elements.

--- Exercises/Exercise1/js/script.js
+++ Exercises/Exercise1/js/script.js
@@ -1,14 +1,31 @@
 /**************************************************
 E1: I like to move it!
 Melissa Banoen-Garde
-
-Exercise 1 for CART253-B. Here I program light and moths.
+CART 253 - B
+Light and moths
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+  background(49, 49, 104);
+
+  //Jar
+  strokeWeight(3);
+  stroke(0,30);
+  fill(255,100);
+  rect(195,200,110,170,30,30,20,20);
+
+  //Lid
+  stroke(0,10);
+  fill(102,102,153);
+  rect(198,200,103,10,90);
+  rect(198,190,103,10,90);
+  rect(198,180,103,10,90);
+
+  noStroke();
 
 }
 
