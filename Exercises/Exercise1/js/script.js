200ad73ae1b6cdaf7f170fc36d8f2b69809f779c
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-21 19:14:17 -0400

    E1: Starting fairy, finished setting lid and jar into JavaScript objects
    Fairy will appear as a glowing blob, controlled by the viewer's cursor

--- Exercises/Exercise1/js/script.js
+++ Exercises/Exercise1/js/script.js
@@ -2,30 +2,48 @@
 E1: I like to move it!
 Melissa Banoen-Garde
 CART 253 - B
-Light and moths
+You (your cursor) are a fairy visiting trapped moths
 **************************************************/
+//Variables
+let jar = {
+  x:195,
+  y:200,
+  w:110,
+  h:170,
+  tlr:30,   //top left radius
+  trr:30,   //top right radius
+  brr:20,   //bottom right radius
+  blr:20,   //bottom left radius
+  fill:255,
+  alpha:100
+
+};
+let lid = {
+
+x:198,
+y:200,
+w:103,
+h:10,
+rad:90,
+r:102,
+g:102,
+b:153
+
+};
+let fairy = {
+
+
+
+};
+
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(500,500);
-  background(49, 49, 104);
 
-  //Jar
-  strokeWeight(3);
-  stroke(0,30);
-  fill(255,100);
-  rect(195,200,110,170,30,30,20,20);
-
-  //Lid
-  stroke(0,10);
-  fill(102,102,153);
-  rect(198,200,103,10,90);
-  rect(198,190,103,10,90);
-  rect(198,180,103,10,90);
-
-  noStroke();
 
 }
 
@@ -33,5 +51,23 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(49, 49, 104);
+
+//Jar
+  strokeWeight(3);
+  stroke(0,30);
+  fill(jar.fill,jar.alpha);
+  rect(jar.x,jar.y,jar.w,jar.h,jar.tlr,jar.trr,jar.brr,jar.blr);
+
+//Lid
+  stroke(0,10);
+  fill(lid.r,lid.g,lid.b);
+  rect(lid.x,lid.y,lid.w,lid.h,lid.r);
+  rect(lid.x,lid.y - 10,lid.w,lid.h,lid.r);
+  rect(lid.x,lid.y - 20,lid.w,lid.h,lid.r);
+  noStroke();
+
+//Fairy
+
 
 }
