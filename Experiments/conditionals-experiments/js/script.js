fac049ab9eeef0dab212eb8d994f5bc782410a30
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-25 18:29:26 -0400

    Conditionals: Learned if statements and relational operators

--- Experiments/conditionals-experiments/js/script.js
+++ Experiments/conditionals-experiments/js/script.js
@@ -1,14 +1,24 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Conditionals Experiments
+Melissa Banoen-Garde
 
-Here is a description of this template p5 project.
+Experimenting with conditionals
 **************************************************/
+
+let backgroundShade = 0;
+let circle = {
+  x:0,
+  y:250,
+  size: 100,
+  speed:5
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+
 
 }
 
@@ -16,5 +26,25 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundShade);
 
+  circle.x = circle.x + circle.speed;
+
+  if (circle.x > width) {
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0) {
+    circle.speed = -circle.speed;
+  }
+
+  if (mouseY < height/2) {
+    fill(255,0,0);
+  }
+
+  if (mouseY > height/2) {
+    fill(0,0,255);
+  }
+
+  ellipse(circle.x, circle.y, circle.size);
 }
