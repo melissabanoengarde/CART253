a947186e03f931d6f4b0148e7516509eea660ca3
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-26 10:07:00 -0400

    Conditionals: Learned 'else if'

--- Experiments/conditionals-experiments/js/script.js
+++ Experiments/conditionals-experiments/js/script.js
@@ -10,7 +10,7 @@ let circle = {
   x:0,
   y:250,
   size: 100,
-  speed:5
+  speed:1
 }
 
 // setup()
@@ -30,21 +30,29 @@ function draw() {
 
   circle.x = circle.x + circle.speed;
 
-  if (circle.x > width) {
-    circle.speed = -circle.speed;
-  }
+  // if (circle.x > width) {
+  //   circle.speed = -circle.speed;
+  // }
+  //
+  // if (circle.x < 0) {
+  //   circle.speed = -circle.speed;
+  // }
+  //
+  // if (mouseY < height/2) {
+  //   fill(255,0,0);
+  // }
+  //
+  // if (mouseY > height/2) {
+  //   fill(0,0,255);
+  // }
 
-  if (circle.x < 0) {
-    circle.speed = -circle.speed;
-  }
+  fill(255,255,255);
 
-  if (mouseY < height/2) {
-    fill(255,0,0);
-  }
-
-  if (mouseY > height/2) {
-    fill(0,0,255);
-  }
+if (circle.x > width/3){
+    if (circle.x < 2 * width/3){
+      fill(225,0,0);
+    }
+}
 
   ellipse(circle.x, circle.y, circle.size);
 }
