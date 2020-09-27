adb9ca1a88fb78dbc0d4fd48f502bebb43f3e7c5
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-26 22:48:28 -0400

    Booleans: Learned about true and false

--- Experiments/conditionals-experiments/js/script.js
+++ Experiments/conditionals-experiments/js/script.js
@@ -4,54 +4,28 @@ Melissa Banoen-Garde
 
 Experimenting with conditionals
 **************************************************/
-
-let backgroundShade = 0;
-let circle = {
-  x:0,
-  y:250,
-  size: 100,
-  speed:1
-}
+let displayCircle = false;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(500,500);
-
-
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(backgroundShade);
 
-  circle.x = circle.x + circle.speed;
+  background(0);
 
-  // if (circle.x > width) {
-  //   circle.speed = -circle.speed;
-  // }
-  //
-  // if (circle.x < 0) {
-  //   circle.speed = -circle.speed;
-  // }
-  //
-  // if (mouseY < height/2) {
-  //   fill(255,0,0);
-  // }
-  //
-  // if (mouseY > height/2) {
-  //   fill(0,0,255);
-  // }
+  if (mouseIsPressed) {
+    displayCircle = true;
+  }
 
-  fill(255,255,255);
-
-if (!(circle.x < width/3)) {
-      fill(225,0,0);
-
+if (displayCircle) {
+  ellipse(250,250,100,100);
 }
 
-  ellipse(circle.x, circle.y, circle.size);
 }
