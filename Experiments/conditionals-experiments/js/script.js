d4ef9c61f139fd8aa9bb3946561df164579cb4bc
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 14:25:14 -0400

    Movement: Learned acceleration and velocity, how to create *fancy* movement

--- Experiments/conditionals-experiments/js/script.js
+++ Experiments/conditionals-experiments/js/script.js
@@ -10,22 +10,43 @@ let circle = {
   y: 250,
   size: 100,
   vx: 0,         //velocity on the x-axis
-  vy: 0          //velocity on the y-axis
+  vy: 0,          //velocity on the y-axis
+
+  //ax and ay are how fast the velocity increases
+  ax: 0,        //acceleration on the x-axis
+  ay: 0,      //acceleration on the y-axis
+  acceleration: 0.25, //how much acceleration adds to the velocity per frame
+  maxSpeed:5
 }
 
-// setup()
-//
-// Description of setup() goes here.
+
 function setup() {
   createCanvas(500,500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
+
 function draw() {
 
   background(0);
+
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  }
+  else {
+    circle.ax = circle.acceleration;
+  }
+
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  }
+  else {
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx += circle.ax;
+  circle.vx = constrain(circle.vx, -circle.maxSpeed,circle.maxSpeed);
+  circle.vy += circle.ay;
+  circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
 
   circle.x += circle.vx;
   circle.y += circle.vy;
