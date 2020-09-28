8c304c5cbb49d3477527a78ddaa5d6e834d57f4f
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 21:52:57 -0400

    A4: Created the covid19 circle

--- Activities/04-dodging-covid-19/js/script.js
+++ Activities/04-dodging-covid-19/js/script.js
@@ -8,12 +8,28 @@ movement, and intermediate drawing. As well as learn
 new functions (dist(), noLoop(), noCursor()).
 **************************************************/
 
+let covid19 = {
+  x:0,
+  y:250,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:5,
+  fill: {
+    r:255,
+    g:0,
+    b:0
+  }
+}
+
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
+
 
 // draw()
 //
