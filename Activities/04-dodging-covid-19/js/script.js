c3aab331a103f251b05a8d85c4f4e62883940061
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 23:23:23 -0400

    A4: Finished Activity 04; Dodging COVID19
    added noCursor()

--- Activities/04-dodging-covid-19/js/script.js
+++ Activities/04-dodging-covid-19/js/script.js
@@ -8,11 +8,12 @@ movement, and intermediate drawing. As well as learn
 new functions (dist(), noLoop(), noCursor()).
 **************************************************/
 
+//VARIABLES
 let user = {
-  x:0,
-  y:250,
-  size:100,
-  fill:255
+  x: 250,
+  y: 250,
+  size: 100,
+  fill: 255
 }
 
 let covid19 = {
@@ -21,7 +22,7 @@ let covid19 = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 5,
+  speed: 15,
   fill: {
     r: 255,
     g: 0,
@@ -29,65 +30,65 @@ let covid19 = {
   }
 }
 
+let numStatic = 1500;
 
+/*************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
+//SETUP
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
 
-  covid19.y = random(0,height);
+  covid19.y = random(0, height);
   covid19.vx = covid19.speed;
 
+  noCursor();
 }
 
+/*************************************************/
 
-// draw()
-//
-// Description of draw() goes here.
+//DRAW
 function draw() {
 
   background(0);
 
-//STATIC
-for(let i = 0; i < 100; i++){
-  let x = random(0,width);
-  let y = random(0,height);
-  stroke(255);
-  point(x,y);
-}
+  //STATIC
+  for (let i = 0; i < numStatic; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    stroke(255);
+    point(x, y);
+  }
 
-//USER'S CIRCLE
+  //USER'S CIRCLE
   //Movement
   user.x = mouseX;
   user.y = mouseY;
 
   //Display
   fill(user.fill);
-  ellipse(user.x,user.y,user.size);
+  ellipse(user.x, user.y, user.size);
 
 
-//COVID19 CIRCLE
+  //COVID19 CIRCLE
   //Movement
   covid19.x += covid19.vx;
   covid19.y += covid19.vy;
 
   if (covid19.x > windowWidth) {
     covid19.x = 0;
-    covid19.y = random(0,windowHeight);
+    covid19.y = random(0, windowHeight);
   }
 
   //Display
-  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
   noStroke();
-  ellipse(covid19.x,covid19.y,covid19.size);
+  ellipse(covid19.x, covid19.y, covid19.size);
 
 
   //Catching COVID19
-  let d = dist(user.x,user.y,covid19.x,covid19.y);
-  if (d < covid19.size/2 + user.size/2) {
+  let d = dist(user.x, user.y, covid19.x, covid19.y);
+  if (d < covid19.size / 2 + user.size / 2) {
     noLoop();
   }
 
