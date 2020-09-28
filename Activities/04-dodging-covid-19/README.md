837fc09c40a605f06fb7f623cd0059d503de2f56
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 21:44:04 -0400

    Starting Activity 04: Dodging COVID-19

---
+++ Activities/04-dodging-covid-19/README.md
@@ -0,0 +1,5 @@
+#Activity 04: Dodging COVID-19
+
+Melissa Banoen-Garde
+CART 253 - B
+2020
---
+++ Activities/04-dodging-covid-19/js/script.js
@@ -0,0 +1,22 @@
+/**************************************************
+Activity 04: Dodging COVID-19
+Melissa Banoen-Garde
+
+This activity implements what we learned in Week 4;
+conditionals, booleans, loops, mouse input,
+movement, and intermediate drawing.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
