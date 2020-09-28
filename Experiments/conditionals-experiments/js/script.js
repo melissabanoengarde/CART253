c46dee5bce1b2106912eb505bed83084eab969f2
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 21:35:53 -0400

    Intermediate Drawing: Learned push( ), pop( ), rotate( ), and translate( )

--- Experiments/conditionals-experiments/js/script.js
+++ Experiments/conditionals-experiments/js/script.js
@@ -4,15 +4,26 @@ Melissa Banoen-Garde
 
 Experimenting with conditionals
 **************************************************/
+let angle = 0;
 
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 }
 
 
 function draw() {
 
-  background(0);
+  background(127);
+
+  push();
+  fill(255,0,0);
+  rectMode(CENTER);
+  translate(width/2,height/2);
+  rotate(angle);
+  rect(width/2,height/2,100,100)
+  pop();
+
+angle += 0.01;
 
 }
