ace2ea3693e46a43e2f0c3daf32183bdbf50d5f3
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 21:46:04 -0400

    A4: Started the COVID-19 activity

--- Activities/04-dodging-covid-19/js/script.js
+++ Activities/04-dodging-covid-19/js/script.js
@@ -4,7 +4,8 @@ Melissa Banoen-Garde
 
 This activity implements what we learned in Week 4;
 conditionals, booleans, loops, mouse input,
-movement, and intermediate drawing.
+movement, and intermediate drawing. As well as learn
+new functions (dist(), noLoop(), noCursor()).
 **************************************************/
 
 // setup()
