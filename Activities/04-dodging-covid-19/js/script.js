d7d5f37e775613f306d9733842b2ad422891846f
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-09-27 22:04:17 -0400

    A4: Set up the canvas, drew background and covid19 circle in draw( )

--- Activities/04-dodging-covid-19/js/script.js
+++ Activities/04-dodging-covid-19/js/script.js
@@ -9,16 +9,16 @@ new functions (dist(), noLoop(), noCursor()).
 **************************************************/
 
 let covid19 = {
-  x:0,
-  y:250,
-  size:100,
-  vx:0,
-  vy:0,
-  speed:5,
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
   fill: {
-    r:255,
-    g:0,
-    b:0
+    r: 255,
+    g: 0,
+    b: 0
   }
 }
 
@@ -27,7 +27,12 @@ let covid19 = {
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500,500);
+
+  createCanvas(windowWidth, windowHeight);
+
+  covid19.y = random(0,height);
+  covid19.vx = covid19.speed;
+  
 }
 
 
@@ -35,5 +40,15 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
+  background(133, 163, 224);
+
+  covid19.x += covid19.vx;
+  covid19.y += covid19.vy;
+
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  noStroke();
+
+  ellipse(covid19.x,covid19.y,covid19.size);
 
 }
