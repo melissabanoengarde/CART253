1dd2de8a8d0a60e8ff5221e51dd5353e0d43210a
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 15:48:09 -0400

    Learned about return values and how to code something more readable

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,20 +1,21 @@
-/**************************************************
-Functions
-Melissa Banoen-Garde
-
-Week 5, modularity and reuse
-**************************************************/
-
-
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 }
 
 function draw() {
   background(0);
+  // We assign the return value of randomColor() into our color variable
+  // so it will have an object with three random color properties
+  let color = randomColor();
+  fill(color.r, color.g, color.b);
+  ellipse(250, 250, 100, 100);
+}
 
-  let x = random(0,width);
-  let y = random (0,height);
-
-  ellipse(x,y,100);
+function randomColor() {
+  let result = {
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255)
+  };
+  return result;
 }
