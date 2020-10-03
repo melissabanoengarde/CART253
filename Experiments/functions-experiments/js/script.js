8130ddb45af50104148f58086094acdcb1304389
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 15:57:48 -0400

    Learning about strings

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,1 +1,15 @@
- 
+
+
+let hamlet1 = "To be or not to be";
+
+let hamlet2 = 'That is the question';   //single quote
+
+let hamlet3 = `Hello nation`;          //backtick
+
+
+
+//Why we should carefully pick how to indicate your strings
+
+//imagine
+let string = "She said, "You can't kill me, i'm a bad b-tch!"";
+//you're confused, javascript is confused, thus use another method
