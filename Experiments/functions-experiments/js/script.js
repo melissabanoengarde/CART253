95deb2b4a1504894fe31ebf854db1dc0df77a750
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 15:07:10 -0400

    Starting 5.3: Functions with return values

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -13,19 +13,8 @@ function setup() {
 function draw() {
   background(0);
 
-  parallels(0, 200, 100, 1, 100, 4);
-  parallels(0, 300, 20, 10, 50, 12);
-  parallels(0, 350, 80, 5, 5, 6);
-}
+  let x = random(0,width);
+  let y = random (0,height);
 
-function parallels(x,y,numLines,lineThickness,lineHeight,lineSpacing) {
-
-  for (let i = 0; i < numLines; i++) {
-    noStroke();
-    let lineFill = map(i, 0 ,numLines, 0,255);
-    fill(lineFill);
-    rectMode(CENTER);
-    rect(x, y, lineThickness, lineHeight);
-    x = x + lineSpacing;
-  }
+  ellipse(x,y,100);
 }
