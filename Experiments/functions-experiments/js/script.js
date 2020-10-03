cc17cd0572a998f796ca24fb1f2c46459a04cd5c
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 12:05:39 -0400

    Starting 5.2: Functions with parameters

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -4,13 +4,7 @@ Melissa Banoen-Garde
 
 Week 5, modularity and reuse
 **************************************************/
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy:0
-}
+
 
 // setup()
 //
@@ -26,42 +20,6 @@ function setup() {
 function draw() {
   background(0);
 
-move();
-wrap();
-display();
 
-
-
-//DEFINING move()
-  function move() {
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-  }
-
-function wrap () {
-  if (circle.x > width) {
-    //CALLING reset()
-    reset();
-  }
-}
-
-//DEFINING display
-function display() {
-  fill(255,0,0);
-  ellipse(circle.x, circle.y, circle.size);
-  }
-}
-
-//DEFINING reset()
-function reset() {
-  circle.x = 0;
-  circle.vx = circle.vx +2;
-  circle.vy -= 0.25; //moves diagonally
-  circle.size +=5;
-
-  function mousePressed() {
-  // CALLING reset()
-  reset();
-  }
 
 }
