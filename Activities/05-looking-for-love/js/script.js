f57a2a20218138a5c782bf4cd75638d938fa5d66
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-04 15:36:17 -0400

    A5: Started the Looking for Love activity

--- Activities/05-looking-for-love/js/script.js
+++ Activities/05-looking-for-love/js/script.js
@@ -4,6 +4,10 @@ Melissa Banoen-Garde
 
 Activity 05 implements all that which we've learned
 about functions, in week 5.
+
+Objective: 1 - Use states to manage your program
+           2 - Use functions to organize your program
+           3 - Write a function with a parameter and return value
 **************************************************/
 
 // setup()
