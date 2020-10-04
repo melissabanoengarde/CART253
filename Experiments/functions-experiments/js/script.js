a03bfed115fbbf828591f2c17206c415a6859e14
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-04 13:35:23 -0400

    Starting video 5.7: Automated movement

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,7 +1,25 @@
-function setup() {
-
+let c = {
+  x: 250,
+  y:250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
 }
 
-function drawing() {
 
+function setup() {
+  createCanvas(500,500);
+
+  c.vx = c.speed;
+  c.vy = c.speed;
+}
+
+function draw() {
+  background(0);
+
+  c.x += c.vx;
+  c.y += c.vy;
+
+  ellipse(c.x,c.y,c.size);
 }
