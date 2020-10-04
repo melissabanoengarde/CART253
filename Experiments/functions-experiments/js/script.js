88cd68c382e512872e3a61b297ea2f51afe886d9
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-04 13:55:47 -0400

    cool jittery behaviour

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -16,10 +16,22 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(100,50,25);
+
+  let change = random();  // when we use random without parameters, it's gonna give back random numbers between 0 and 1
+  if (change < 0.05) {     //  this will be true 10% of the time
+    c.vx = random(-c.speed,c.speed);
+    c.vy = random(-c.speed,c.speed);
+}
 
   c.x += c.vx;
   c.y += c.vy;
 
   ellipse(c.x,c.y,c.size);
 }
+
+
+
+
+// the numbers that come out of the random function, follow a uniform distribution
+// every number is just as likely to pop out whenever we call it
