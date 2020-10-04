580115cdbc050265a45a760c6d0adeb954a1ccb6
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 23:41:03 -0400

    Starting 5.7: Automated Movement

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,21 +1,7 @@
-let typing = ``; // Empty string to begin with
-
 function setup() {
-  createCanvas(500, 500);
+
 }
 
-function draw() {
-  background(0);
+function drawing() {
 
-  // Display our cumulative typing variable on the canvas...
-  fill(255);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  text(typing, width / 2, height / 2);
-}
-
-function keyTyped() {
-  // Whenever a "typeable" key is pressed, add the most recent key to our typing string
-  // (Using + with two strings like this is called concatenation, adding them together)
-  typing = typing + key;
 }
