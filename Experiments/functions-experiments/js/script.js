71fe6ad3cbc43e6e87edc5bbceae61ccaaaaf02f
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 23:19:00 -0400

    Learning the important of key codes
    keycode.info

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,9 +1,25 @@
 let bg = 0;
 
-function setup (){
-
+function setup() {
+  createCanvas(500,500);
 }
 
-function draw(){
-  background(0);
+function draw() {
+  background(bg);
+
+  textAlign(CENTER,CENTER);
+  textSize(64);
+  fill(255);
+  text(keyCode, width/2,height/2);  //ascii key codes
+}
+
+function keyPressed() {  // a function p5 already knows about
+  if (keyCode === UP_ARROW) {    //38 is the up arrow, refer to keycode.info website
+    bg += 10;
+    bg = constrain(bg,0,255);
+  }
+  else if (keyCode === DOWN_ARROW){
+    bg -= 10;
+    bg = constrain(bg,0,255);
+  }
 }
