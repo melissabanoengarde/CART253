6db9fa0e153aab7d32cb2bbe11d47d53432847a8
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-03 22:17:34 -0400

    Starting video 5.5: States

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -1,35 +1,24 @@
-let hello ={
-  string: `Hello, wold!`,
+let c = {
   x:0,
   y:250,
-  vx: 5,
-  vy: 1,
-  size: 64
+  size:100,
+  vx: 0,
+  vy: 0,
+  speed:2
 };
 
 
 function setup(){
   createCanvas(500,500);
+  c.vx += c.speed;
 }
 
 function draw(){
-  background(127);
+  background(0);
 
-  hello.x += hello.vx;
-  hello.y += hello.vy;
+  c.x += c.vx;
+  c.y += c.vy;
 
-  hello.size += 1;
-
-  textAlign(CENTER,CENTER);
-  textSize(hello.size);
-  textStyle(ITALIC);
-
-  fill(250,150,100);
-  strokeWeight(5);
-  stroke(255);
-
-  text(hello.string, hello.x,hello.y)        //we are specifying a string at this (250,250) position
+  ellipse(c.x,c.y,c.size);
 
 }
-
-//can look up other fonts, check it out in reference
