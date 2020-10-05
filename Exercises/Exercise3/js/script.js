ccb08fc27f24607f4a93ec846c6bef9a691c2861
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-05 18:33:23 -0400

    E3: Now adding flower in which bee must reach in order to win

--- Exercises/Exercise3/js/script.js
+++ Exercises/Exercise3/js/script.js
@@ -9,64 +9,64 @@ OCT. 2020
 
 // Variables
 let beeImg;
-  let bee = {
-    x:400,
-    y:250,
-    size:100,
-    vx:0,
-    vy:0,
-    speed: 5,
-    img: beeImg
-  };
+let bee = {
+  x: 300,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  img: beeImg
+};
 
 let bird1Img;
-  let bird1 = {
-    x:20,
-    y:200,
-    size:100,
-    vx:0,
-    vy:0,
-    speed:4.5,
-    img: bird1Img
-  }
+let bird1 = {
+  x: 20,
+  y: 200,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 4.5,
+  img: bird1Img
+}
 
 let bird2Img;
-  let bird2 = {
-    x:50,
-    y:300,
-    size:100,
-    vx:0,
-    vy:0,
-    speed:4.5,
-    img: bird2Img
-  }
+let bird2 = {
+  x: 50,
+  y: 300,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 4.5,
+  img: bird2Img
+}
 
 let bird3Img;
-  let bird3 = {
-    x:30,
-    y:100,
-    size:100,
-    vx:0,
-    vy:0,
-    speed:4.5,
-    img: bird3Img
-  }
+let bird3 = {
+  x: 30,
+  y: 100,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 4.5,
+  img: bird3Img
+}
 
 let bird4Img;
-  let bird4 = {
-      x:20,
-      y:250,
-      size:100,
-      vx:0,
-      vy:0,
-      speed:5,
-      img: bird4Img
-    }
+let bird4 = {
+  x: 20,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  img: bird4Img
+}
 
 let bg;
 let bgLeft = 0;
 let bgRight = 800;
-let bgSpeed = 3;
+let bgSpeed = 2;
 
 
 function preload() {
@@ -80,12 +80,12 @@ function preload() {
 
 
 function setup() {
-  createCanvas(800,500);
+  createCanvas(800, 500);
 
   setupBirds();
 }
 
-function setupBirds(){
+function setupBirds() {
   bird1.vx = random(0.02, bird1.speed);
   bird2.vx = random(0.5, bird2.speed);
   bird3.vx = random(0.8, bird3.speed);
@@ -97,86 +97,95 @@ function draw() {
   beeMove();
   birdsMove();
   handleInput();
-  scrollingBackground();    //remember, order MATTERS
+  scrollingBackground(); //remember, order MATTERS
+  overLap();
   display();
 
-// Bee's key info (AWSD)
- function handleInput(){
-   // Bee movements
-   if (keyIsDown(65)){
-         bee.vx = -bee.speed;
-       }
+  // Bee's key info (AWSD)
+  function handleInput() {
+    // Bee movements
+    if (keyIsDown(65)) {
+      bee.vx = -bee.speed;
+    } else if (keyIsDown(68)) {
+      bee.vx = bee.speed;
+    } else {
+      bee.vx = 0;
+    }
 
-     else if (keyIsDown(68)){
-         bee.vx = bee.speed;
-       }
-
-     else {
-         bee.vx = 0;
-       }
-
-   if (keyIsDown(87)){
-        bee.vy = -bee.speed;
-       }
-
-     else if (keyIsDown(83)){
-        bee.vy = bee.speed;
-      }
-
-      else {
-         bee.vy = 0;
-       }
- }
-
-
- function beeMove() {
-   bee.x += bee.vx;
-   bee.y += bee.vy;
- }
-
- function birdsMove(){
-   // Bird 1
-   bird1.x += bird1.vx;
-   bird1.y += bird1.vy;
-
-   // Bird 2
-   bird2.x += bird2.vx;
-   bird2.y += bird2.vy;
-
-   // Bird 3
-   bird3.x += bird3.vx;
-   bird3.y += bird3.vy;
-
-   // Bird 4
-   bird4.x += bird4.vx;
-   bird4.y += bird4.vy;
- }
-
-
- function scrollingBackground() {
-  // Background Scrolling
-  imageMode(CORNER);
-  image(bg,bgLeft,0,width,height);      // Left
-  image(bg,bgRight,0,width,height);     // Right
-
-  bgLeft -= bgSpeed;
-  bgRight -= bgSpeed;
-
-  if (bgLeft < -width) {
-    bgLeft = bgRight + width;
+    if (keyIsDown(87)) {
+      bee.vy = -bee.speed;
+    } else if (keyIsDown(83)) {
+      bee.vy = bee.speed;
+    } else {
+      bee.vy = 0;
+    }
   }
-  if (bgRight < -width) {
-    bgRight = bgLeft -bgRight;
+
+
+  function beeMove() {
+    bee.x += bee.vx;
+    bee.y += bee.vy;
   }
-}
+
+  function birdsMove() {
+    // Bird 1
+    bird1.x += bird1.vx;
+    bird1.y += bird1.vy;
+
+    // Bird 2
+    bird2.x += bird2.vx;
+    bird2.y += bird2.vy;
+
+    // Bird 3
+    bird3.x += bird3.vx;
+    bird3.y += bird3.vy;
+
+    // Bird 4
+    bird4.x += bird4.vx;
+    bird4.y += bird4.vy;
+  }
 
 
- function display(){
-   image(beeImg, bee.x, bee.y, bee.size,bee.size);
-   image(bird1Img, bird1.x, bird1.y, bird1.size,bird1.size);
-   image(bird2Img, bird2.x, bird2.y, bird2.size,bird2.size);
-   image(bird3Img, bird3.x, bird3.y, bird3.size,bird3.size);
-   image(bird4Img, bird4.x, bird4.y, bird4.size,bird4.size);
- }
+  function scrollingBackground() {
+    // Background Scrolling
+    imageMode(CORNER);
+    image(bg, bgLeft, 0, width, height); // Left
+    image(bg, bgRight, 0, width, height); // Right
+
+    bgLeft -= bgSpeed;
+    bgRight -= bgSpeed;
+
+    if (bgLeft < -width) {
+      bgLeft = bgRight + width;
+    }
+    if (bgRight < -width) {
+      bgRight = bgLeft - bgRight;
+    }
+  }
+
+  //When either birds reaches the busy bee
+  function overLap() {
+    let d = dist(bee.x, bee.y, bird1.x, bird1.y);
+    let d2 = dist(bee.x, bee.y, bird2.x, bird2.y);
+    let d3 = dist(bee.x, bee.y, bird3.x, bird3.y);
+    let d4 = dist(bee.x, bee.y, bird4.x, bird4.y);
+
+    if (d < bird1.size / 3 + bee.size / 3 || d2 < bird2.size / 3 + bee.size / 3 || d3 < bird3.size / 3 + bee.size / 3 || d4 < bird4.size / 3 + bee.size / 3) {
+      noLoop();
+      //state = `Bee gone`
+    }
+  }
+
+
+  function display() {
+    imageMode(CENTER);
+    image(beeImg, bee.x, bee.y, bee.size, bee.size);
+    image(bird1Img, bird1.x, bird1.y, bird1.size, bird1.size);
+    image(bird2Img, bird2.x, bird2.y, bird2.size, bird2.size);
+    image(bird3Img, bird3.x, bird3.y, bird3.size, bird3.size);
+    image(bird4Img, bird4.x, bird4.y, bird4.size, bird4.size);
+
+    // ellipse(bird2.x, bird2.y, bird2.size);
+  }
 
 }
