e57ee594a15b3873bd763bd356223b99d94a7b5c
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-05 10:12:45 -0400

    E3: Restarting exercise 3

--- Exercises/Exercise3/js/script.js
+++ Exercises/Exercise3/js/script.js
@@ -11,54 +11,54 @@ Moving background: https://editor.p5js.org/chjno/sketches/ByZlypKWM
 **************************************************/
 
 // Variables
-let bgPic;
-let bgLeft = 0;
-let moveSpeed = 5;
-let b;
-
-function preload() {
-  bgPic = loadImage("assets/images/E3bg.png");
-}
-
-function setup() {
-  createCanvas(800, 500);
-  rectMode(CENTER);
-
-}
-
-function draw() {
-  background(0);
-  image(bgPic, bgLeft, 0);
-
-  keyPressed();
-
-  // b.show();
-}
-
-function moveBgLeft(){
-
-  let minBgLeft = -bg.width + width;
-
-    if (bgLeft - moveSpeed > minBgLeft){
-      bgLeft -= moveSpeed;
-  }
-}
-
-function moveBgRight(){
-  if (bgLeft + moveSpeed < 0){
-    bgLeft += moveSpeed;
-  }
-}
-
-function keyPressed() {
-  if (keyCode === 65){    //Move to left
-
-
-  }
-}
-
-
-}
+// let bgPic;
+// let bgLeft = 0;
+// let moveSpeed = 5;
+// let b;
+//
+// function preload() {
+//   bgPic = loadImage("assets/images/E3bg.png");
+// }
+//
+// function setup() {
+//   createCanvas(800, 500);
+//   rectMode(CENTER);
+//
+// }
+//
+// function draw() {
+//   background(0);
+//   image(bgPic, bgLeft, 0);
+//
+//   keyPressed();
+//
+//   // b.show();
+// }
+//
+// function moveBgLeft(){
+//
+//   let minBgLeft = -bg.width + width;
+//
+//     if (bgLeft - moveSpeed > minBgLeft){
+//       bgLeft -= moveSpeed;
+//   }
+// }
+//
+// function moveBgRight(){
+//   if (bgLeft + moveSpeed < 0){
+//     bgLeft += moveSpeed;
+//   }
+// }
+//
+// function keyPressed() {
+//   if (keyCode === 65){    //Move to left
+//
+//
+//   }
+// }
+//
+//
+// }
 
 
 
