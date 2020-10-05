2609551593326ff9a92e65e4285dec7f9ede6336
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-05 15:34:42 -0400

    E3: This whole time, I could not uundertand why my bee was not moving, especially since I have been referring to the notes I took throughout thee course of chapter 5's videos. It turns out, I was missing a curly bracket.

--- Exercises/Exercise3/js/script.js
+++ Exercises/Exercise3/js/script.js
@@ -5,31 +5,29 @@ Melissa Banoen-Garde
 WEEK 05: FUNCTIONS
 CART 253 - B
 OCT. 2020
-
-Source:
-Moving background: https://editor.p5js.org/chjno/sketches/ByZlypKWM
 **************************************************/
 
 // Variables
-let bg;
-let bgLeft = 0;
-let bgRight = 800;
-let bgSpeed = 3;
+let beeImg;
+  let bee = {
+    x:400,
+    y:250,
+    size:100,
+    vx:0,
+    vy:0,
+    speed: 5,
+    img: beeImg
+  };
 
-let bee = {
-  x:400,
-  y:400,
-  size:100,
-  vx:0,
-  vy:0,
-  speed: 2,
-  img:'abeille'
-};
+// let bg;
+// let bgLeft = 0;
+// let bgRight = 800;
+// let bgSpeed = 3;
 
 
 function preload() {
-  bg = loadImage('assets/images/bg.png');
-  abeille  = loadImage('assets/images/bee2.png');
+  // bg = loadImage('assets/images/bg.png');
+  beeImg = loadImage('assets/images/bee.png');
 }
 
 
@@ -41,24 +39,36 @@ function setup() {
 function draw() {
   background(0);
 
-  // scrollingBackground();
 
-  bee.x = bee.x + bee.vx;
-  bee.y = bee.y + bee.vy;
+  if (keyIsDown(65)){
+        bee.vx = -bee.speed;
+      }
+    else if (keyIsDown(68)){
+        bee.vx = bee.speed;
+      }
+    else {
+        bee.vx = 0;
+      }
 
-  // if (keyIsDown(68)){
-  //   bee.vx += bee.speed;
-  // }
-  // else if (keyIsDown(65)){
-  //   bee.vx -= bee.speed;
-  // }
-  // else {
-  //   bee.vx = 0;
-  // }
+      if (keyIsDown(87)){
+            bee.vy = -bee.speed;
+          }
+        else if (keyIsDown(83)){
+            bee.vy = bee.speed;
+          }
+        else {
+            bee.vy = 0;
+          }
 
-  image(abeille, bee.x, bee.y, bee.size, bee.size);
+      bee.x += bee.vx;
+      bee.y += bee.vy;
+
+  image(beeImg, bee.x, bee.y, bee.size);
 
 
+//   scrollingBackground();
+//
+//
 //  function scrollingBackground(){
 //   // Background Scrolling
 //   imageMode(CORNER);
@@ -74,13 +84,6 @@ function draw() {
 //   if (bgRight < -width) {
 //     bgRight = bgLeft -bgRight;
 //   }
-// }
-
 }
-
-
-// IDEA: image(CORNER)????
-// The default mode is imageMode(CORNER), which interprets the second and
-// third parameters of image() as the upper-left corner of the image.
-// If two additional parameters are specified, they are used to set the image's
-// width and height.   image(img, x, y, [width], [height])
+//
+//
