51677b1017c4c30f3508369a22fac2fb2a98dbc1
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2020-10-05 14:12:35 -0400

    E3: Finally figured out to have my bee png appear
    Had to add single quotes around "abeille": img: 'abeille'

--- Exercises/Exercise3/assets/images/README.md
+++ Exercises/Exercise3/assets/images/README.md
@@ -5,3 +5,7 @@ Source:
 Bee - https://www.uihere.com/free-cliparts/bee-insect-clip-art-cartoon-bee-png-1066085/download
 
 Background - https://free-game-assets.itch.io/free-cartoon-forest-2d-backgrounds
+
+Purple martin bird - http://www.creativeforkids.com/purple-martin-bird.html
+
+Scarlet tanager bird - 
--- Exercises/Exercise3/js/script.js
+++ Exercises/Exercise3/js/script.js
@@ -17,52 +17,66 @@ let bgRight = 800;
 let bgSpeed = 3;
 
 let bee = {
-  x:0,
-  y:0,
-  size:0,
+  x:400,
+  y:400,
+  size:100,
   vx:0,
   vy:0,
   speed: 2,
-  img: bee
-}
+  img:'abeille'
+};
+
 
 function preload() {
   bg = loadImage('assets/images/bg.png');
-  bee = loadImage('assets/images/bee.png');
+  abeille  = loadImage('assets/images/bee2.png');
 }
 
 
 function setup() {
   createCanvas(800,500);
 
-
-
 }
 
 function draw() {
+  background(0);
 
-  scrollingBackground();
+  // scrollingBackground();
+
+  bee.x = bee.x + bee.vx;
+  bee.y = bee.y + bee.vy;
+
+  // if (keyIsDown(68)){
+  //   bee.vx += bee.speed;
+  // }
+  // else if (keyIsDown(65)){
+  //   bee.vx -= bee.speed;
+  // }
+  // else {
+  //   bee.vx = 0;
+  // }
+
+  image(abeille, bee.x, bee.y, bee.size, bee.size);
+
+
+//  function scrollingBackground(){
+//   // Background Scrolling
+//   imageMode(CORNER);
+//   image(bg,bgLeft,0,width,height);      // Left
+//   image(bg,bgRight,0,width,height);     // Right
+//
+//   bgLeft -= bgSpeed;
+//   bgRight -= bgSpeed;
+//
+//   if (bgLeft < -width) {
+//     bgLeft = bgRight + width;
+//   }
+//   if (bgRight < -width) {
+//     bgRight = bgLeft -bgRight;
+//   }
+// }
 
 }
-
-function scrollingBackground(){
-  // Background Scrolling
-  imageMode(CORNER);
-  image(bg,bgLeft,0,width,height);      // Left
-  image(bg,bgRight,0,width,height);     // Right
-
-  bgLeft -= bgSpeed;
-  bgRight -= bgSpeed;
-
-  if (bgLeft < -width) {
-    bgLeft = bgRight + width;
-  }
-  if (bgRight < -width) {
-    bgRight = bgLeft -bgRight;
-  }
-
-}
-
 
 
 // IDEA: image(CORNER)????
