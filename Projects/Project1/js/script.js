ee29833be0008fb392535038bc8ec7d8b9c5f180
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-11 12:57:42 -0400

    P1: Set typeFace's key input so user can move. I also added its acceleration and maxSpeed
    The purpose of the acceleration and maximum speed is to increase the user's difficulty in controlling their object and, equally, avoiding objects to steer away from (which I will set soon). The acceleration, ax, ay, and maxSpeed is repurposed from Exercise 2: Dodge-Em.

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -9,14 +9,18 @@ Here is a description of this template p5 project.
 let bg;
 
 // typeFace specs
-let typeFace;
+let typeFaceImg;
   let typeFace = {
-    x:0,
-    y:0,
-    size:100,
+    x:750,
+    y:450,
+    size:40,
     vx:0,
     vy:0,
-    speed: 5
+    speed: 5,
+    ax:0,
+    ay:0,
+    acceleration: 6,
+    maxSpeed: 1
   };
 
 
@@ -24,7 +28,7 @@ let typeFace;
 
 function preload(){
   bg = loadImage("assets/images/bg.jpg");
-  typeFace = loadImage("assets/images/bg.jpg");
+  typeFaceImg = loadImage("assets/images/typeFace.png");
 }
 
 
@@ -45,10 +49,41 @@ function draw() {
 
 
   // typeFace
+
   typeFace.x += typeFace.vx;
   typeFace.y += typeFace.vy;
 
+  typeFace.vx += typeFace.ax;
+  typeFace.vy += typeFace.ay;
+
   // key input AWSD
-  if ()
+  if (keyIsDown(65)) {                 // A = Left
+    typeFace.vx = -typeFace.speed;
+    typeFace.ax = -typeFace.acceleration;
+  }
+  else if (keyIsDown(68)) {            // D = Right
+    typeFace.vx = typeFace.speed;
+    typeFace.ax = typeFace.acceleration;
+  }
+  else {
+    // typeFace.vx = 0;                   // When nothing is pressed, typeFace stops moving
+    typeFace.vx = constrain(typeFace.vx, -typeFace.maxSpeed, typeFace.maxSpeed);
+  }
+
+  if (keyIsDown(87)) {                // W = Up
+    typeFace.vy = -typeFace.speed;
+    typeFace.ay = -typeFace.acceleration;
+  }
+  else if (keyIsDown(83)) {           // S = Down
+    typeFace.vy = typeFace.speed;
+    typeFace.ay = typeFace.acceleration;
+  }
+  else {                              // When nothing is pressed, typeFace stops moving
+    // typeFace.vy = 0;
+    typeFace.vy = constrain(typeFace.vy, -typeFace.maxSpeed, typeFace.maxSpeed);
+  }
+
+  // display
+  image(typeFaceImg, typeFace.x, typeFace.y, typeFace.size, typeFace.size);
 
 }
