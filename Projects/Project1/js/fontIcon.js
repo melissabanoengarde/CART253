e2721351f1f43c2da1551531250a63bca2d3b75d
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-11 16:25:50 -0400

    P1: Programming "typeFace" and background interactivity. Created a class for typeFace in a new JavaScript file named "fontIcon".
    Unfortunately, my initial thought/idea on how to make the background *move* with the typeFace object did not work. However, I found a way to make this idea come to (cyber) life. I will provide the link I am currently following in the README.md file, under "sources and references". As for now, I've noted my external resources in a textEdit document. So far, I've learned about "class", "this.", and "constructor".

---
+++ Projects/Project1/js/fontIcon.js
@@ -0,0 +1,38 @@
+class fontIcon {
+
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.size = 20;
+    // this.vx = 0;
+    // this.vy = 0;
+    // this.speed = moveSpeed;     // previously 5
+    // this.ax = 0;
+    // this.ay = 0;
+    // this.acceleration = 6;
+    // this.maxSpeed = 1
+  };
+
+
+  show(){
+    rect(this.x,this.y,this.size,this.size);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*NOTES
++ When you define a class you can make it do whatever you want (6:42)
+*/
--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -6,84 +6,83 @@ Melissa Banoen-Garde
 
 Here is a description of this template p5 project.
 **************************************************/
-let bg;
-
-// typeFace specs
-let typeFaceImg;
-  let typeFace = {
-    x:750,
-    y:450,
-    size:40,
-    vx:0,
-    vy:0,
-    speed: 5,
-    ax:0,
-    ay:0,
-    acceleration: 6,
-    maxSpeed: 1
-  };
+let bg //2272 x 881
+let typeFace;
 
 
-
-
-function preload(){
+function preload() {
   bg = loadImage("assets/images/bg.jpg");
-  typeFaceImg = loadImage("assets/images/typeFace.png");
 }
 
 
-
-
 function setup() {
-  createCanvas(800,500);
+  createCanvas(900, 500);
+  rectMode(CENTER);
 
+  typeFace = new fontIcon(width - 10, height -10);
 }
 
 
 
 function draw() {
   // Background
-  background(bg);
+  background(0);
+  image(bg,0,0);
+
+  typeFace.show();
+
+  //===== typeFace =====/
+  //movement
+  // typeFace.x += typeFace.vx;
+  // typeFace.y += typeFace.vy;
+  //
+  // typeFace.vx += typeFace.ax;
+  // typeFace.vy += typeFace.ay;
+  //
+  // // key input AWSD
+  // if (keyIsDown(65)) {                    // A = Left
+  //   typeFace.vx = -typeFace.speed;
+  //   typeFace.ax = -typeFace.acceleration;
+  // } else if (keyIsDown(68)) {             // D = Right
+  //   typeFace.vx = typeFace.speed;
+  //   typeFace.ax = typeFace.acceleration;
+  // } else {
+  //   // typeFace.vx = 0;                   // When nothing is pressed, typeFace stops moving
+  //   typeFace.vx = constrain(typeFace.vx, -typeFace.maxSpeed, typeFace.maxSpeed);
+  // }
+  //
+  // if (keyIsDown(87)) {                    // W = Up
+  //   typeFace.vy = -typeFace.speed;
+  //   typeFace.ay = -typeFace.acceleration;
+  // } else if (keyIsDown(83)) {             // S = Down
+  //   typeFace.vy = typeFace.speed;
+  //   typeFace.ay = typeFace.acceleration;
+  // } else {                                // When nothing is pressed, typeFace stops moving
+  //   // typeFace.vy = 0;
+  //   typeFace.vy = constrain(typeFace.vy, -typeFace.maxSpeed, typeFace.maxSpeed);
+  // }
+  //
+  // // display
+  // image(typeFaceImg, typeFace.x, typeFace.y, typeFace.size, typeFace.size);
+
+
+
+  //===== Background interaction =====//
   // imageMode(CORNER);
+  // image(bg, bgBottomLeft, 0);
+  //
+  // //When typeFace moves to the bottom left of background
+  // let minBgBottomLeft = -bg.width + width;
+  //
+  // if (bgBottomLeft - typeFace.x > minBgBottomLeft){
+  //   bgBottomLeft -= typeFace.x;
+  // }
+  //
+  // //When typeFace moves to the bottom right of Background
+  // if (bgBottomLeft + typeFace.x < 0){
+  //   bgBottomLeft += typeFace.x;
+  // }
 
 
-
-  // typeFace
-
-  typeFace.x += typeFace.vx;
-  typeFace.y += typeFace.vy;
-
-  typeFace.vx += typeFace.ax;
-  typeFace.vy += typeFace.ay;
-
-  // key input AWSD
-  if (keyIsDown(65)) {                 // A = Left
-    typeFace.vx = -typeFace.speed;
-    typeFace.ax = -typeFace.acceleration;
-  }
-  else if (keyIsDown(68)) {            // D = Right
-    typeFace.vx = typeFace.speed;
-    typeFace.ax = typeFace.acceleration;
-  }
-  else {
-    // typeFace.vx = 0;                   // When nothing is pressed, typeFace stops moving
-    typeFace.vx = constrain(typeFace.vx, -typeFace.maxSpeed, typeFace.maxSpeed);
-  }
-
-  if (keyIsDown(87)) {                // W = Up
-    typeFace.vy = -typeFace.speed;
-    typeFace.ay = -typeFace.acceleration;
-  }
-  else if (keyIsDown(83)) {           // S = Down
-    typeFace.vy = typeFace.speed;
-    typeFace.ay = typeFace.acceleration;
-  }
-  else {                              // When nothing is pressed, typeFace stops moving
-    // typeFace.vy = 0;
-    typeFace.vy = constrain(typeFace.vy, -typeFace.maxSpeed, typeFace.maxSpeed);
-  }
-
-  // display
-  image(typeFaceImg, typeFace.x, typeFace.y, typeFace.size, typeFace.size);
 
 }
