cb4b8ec62d02581ef427a5015247ac675787acad
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-11 11:45:54 -0400

    P1: Setting typeFace's user-input (AWSD)
    I will set typeFace's user-input using conditional statements; if, else, and else if statements.

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -6,17 +6,49 @@ Melissa Banoen-Garde
 
 Here is a description of this template p5 project.
 **************************************************/
+let bg;
 
-// setup()
-//
-// Description of setup() goes here.
+// typeFace specs
+let typeFace;
+  let typeFace = {
+    x:0,
+    y:0,
+    size:100,
+    vx:0,
+    vy:0,
+    speed: 5
+  };
+
+
+
+
+function preload(){
+  bg = loadImage("assets/images/bg.jpg");
+  typeFace = loadImage("assets/images/bg.jpg");
+}
+
+
+
+
 function setup() {
+  createCanvas(800,500);
 
 }
 
-// draw()
-//
-// Description of draw() goes here.
+
+
 function draw() {
+  // Background
+  background(bg);
+  // imageMode(CORNER);
+
+
+
+  // typeFace
+  typeFace.x += typeFace.vx;
+  typeFace.y += typeFace.vy;
+
+  // key input AWSD
+  if ()
 
 }
