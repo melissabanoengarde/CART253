3f55ae3cc6347544cd002e95cbda4714cf5e22c8
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-12 01:28:30 -0400

    P1: Everything I've accomplished in "class fontIcon{}" today, I converted into "functions".
    I did this because my typeFace object's x and y values were always returning as “undefined” when debugging. I figured I was venturing in unfamiliar territory with all the google searches I've done. Results were within creating vectors or something about static fields.
    
    I did a lot of back and forth between p5’s web editor (to convert from JS class to functions), while referencing what I had already programmed in Atom and my previous exercises on GitHub.
    
    I will set up the rest tomorrow.

---
+++ Projects/Project1/js/scriptredo.js
@@ -0,0 +1,168 @@
+"use strict"
+/**************************************************
+ Project 01: Simulation
+ Melissa Banoen-Garde
+
+ Here is a description of this template p5 project.
+**************************************************/
+
+//VARIABLES
+let bg;
+let bgLeft = -1272;
+let bgUp = -280;
+
+let typeFaceImg;
+let typeFace = {
+  x:950,
+  y:520,
+  size: 45,
+  vx:0,
+  speed: 3,
+  vy:0
+};
+
+
+let fontFolderImg;
+let fontFolder = {
+  x:0,
+  y:0,
+  size:55,
+};
+
+function preload (){
+  bg = loadImage("assets/images/bg.jpg");
+  typeFaceImg = loadImage("assets/images/typeFace.png");
+  fontFolderImg = ("assets/images/folder.png");
+}
+
+function setup() {
+  createCanvas(1000, 600);
+
+}
+
+function draw() {
+  background(0);
+  image(bg,bgLeft,bgUp);
+
+
+// Background Movement
+
+function moveBgLeft(){
+  let minBgLeft = -bg.width + width;
+
+  if (bgLeft - typeFace.speed > minBgLeft){
+    bgLeft -= typeFace.speed;
+  }
+}
+
+function moveBgRight(){
+  if (bgLeft + typeFace.speed < 0){
+    bgLeft += typeFace.speed;
+  }
+}
+
+function moveBgUp(){
+  if (bgUp - typeFace.speed < 0){
+    bgUp -= typeFace.speed;
+  }
+}
+
+function moveBgDown(){
+  let minBgUp = -bg.height + height;
+
+  if (bgUp + typeFace.speed > minBgUp){
+    bgUp += typeFace.speed;
+  }
+}
+
+//============================================//
+
+function canGoLeft(){
+  typeFace.x -= typeFace.speed;
+
+  if (typeFace.x > 10 + typeFace.speed){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function canGoRight(){
+  typeFace.x += typeFace.speed;
+
+   if (typeFace.vx < width - (10+typeFace.speed)){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function canGoUp(){
+  typeFace.y -= typeFace.speed;
+
+ if (typeFace.y > 10 + typeFace.speed){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function canGoDown(){
+  typeFace.y += typeFace.speed;
+
+  if (typeFace.y < height - (10+typeFace.speed)){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+// ========== TYPEFACE ========== //
+
+// Key Input
+  //A = LEFT
+  if (keyIsDown(65)) {
+    canGoLeft();
+    moveBgRight();
+  }
+  else if (keyIsDown(68)) {  //D = RIGHT
+    canGoRight();
+    moveBgLeft();
+  }
+  // else {
+  //   typeFace.vx = 0;       //Pressing nothing
+  // }
+
+  else if (keyIsDown(87)) {      //W = UP
+    canGoUp();
+    moveBgDown();
+  }
+  else if (keyIsDown(83)) { //S = DOWN
+    canGoDown();
+    moveBgUp();
+  }
+  // else {
+  //   typeFace.vy = 0;        //Pressing nothing
+  // }
+
+
+
+// Movement
+  // Left
+  // typeFace.x -= typeFace.speed;
+  // // Right
+  // typeFace.x += typeFace.speed;
+  // // Up
+  // typeFace.y += typeFace.speed;
+  // //  Down
+  // typeFace.y -= typeFace.speed;
+
+// Display
+  image(typeFaceImg, typeFace.x,typeFace.y,typeFace.size,typeFace.size);
+
+  }
