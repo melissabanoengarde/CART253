0ad162ea940bdd0cfcfd23fb396518d30ff6960f
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-16 17:49:12 -0400

    Starting to experiment with video 7.1: Intermediate functions

---
+++ Experiments/arrays-experiments/README.md
@@ -0,0 +1,5 @@
+# Week 6: Experimenting with arrays
+
+Melissa Banoen-Garde
+CART 253 - B
+October 2020
---
+++ Experiments/arrays-experiments/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/**************************************************
+Week 6: Arrays
+Melissa Banoen-Garde
+
+Experimenting with arrays.
+**************************************************/
+
+// our user, to move with the mouse
+let user = {
+  x:0,
+  y:0,
+  size:100
+};
+
+// let first food object
+let food1 = {
+  x: 250,
+  y:300,
+  size: 50,
+  eaten: false // We want to track whether the user has eaten the food
+};
+
+let food2 = {
+  x: 350,
+  y:300,
+  size: 50,
+  alive: false
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+
+}
