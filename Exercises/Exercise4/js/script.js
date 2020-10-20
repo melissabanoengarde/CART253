3a18178b96211aa4e373dd59dc0c18687fb62e92
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-20 16:26:28 -0400

    E4: Setting a "end of week" bar (thin rectangle)
    User needs to touch this bar in order to win the game

--- Exercises/Exercise4/js/script.js
+++ Exercises/Exercise4/js/script.js
@@ -9,156 +9,51 @@ Using arrays and all that was covered in week 6.
 
 // User's specs
 let red = {
-  x: 0,
-  y: 0,
+  x: 200,
+  y:15,
   size: 30,
   vx: 0,
   vy:0,
-  speed: 4,
-  trail: [],
-  trailSize: 7
+  speed: 5,
 };
 
-// Food's specs
-let mass = [];
-let massAmount = 15;
 
-
-function setup() {
-  createCanvas(500, 500);
-
-// movement of blues all over screen
-  for (let i = 0; i < massAmount; i++) {
-    mass[i] = createBlues(random(0,width), random(0,height));
-  }
+function setup(){
+  createCanvas(400,900);
 }
 
-// Creates BLUES
-function createBlues(x,y) {
-  let food = {
-    x: x,
-    y: y,
-    size: 15,
-    vx: 0,
-    vy: 0,
-    eaten: false
-  };
-  return food;
-}
 
-/*----------------------------------------------------------------------*/
-function draw() {
+function draw(){
   background(245,245,245);
   noStroke();
 
-  userInput();
-  userMove();
-  trailPosition();
-  moveFood();
-  displayUser();
-
-  for (let i = 0; i < food.length; i++) {
-    moveFood(foodAmount[i]);
-    displayFood(food[i]);
-  }
-}
-
-  // Check whether the user has eaten either food
-  checkFood(food);
-
-  // Display the user and foods
-  displayFood(food);
-
-
-// Sets the user position to the mouse position
-function userInput() {
+/* RED (USER)*/
+  //User's handle input
   if (keyIsDown(65)) {         //A = Left
-     user.vx = -user.speed;
+     red.vx = -red.speed;
    }
    else if (keyIsDown(68)) {  //D = Right
-     user.vx = user.speed;
+     red.vx = red.speed;
    }
    else {
-     user.vx = 0;             //halt
+     red.vx = 0;             //halt
    }
 
    if (keyIsDown(87)) {      //W = Up
-     user.vy = -user.speed;
+     red.vy = -red.speed;
    }
    else if (keyIsDown(83)) { //S = Down
-     user.vy = user.speed;
+     red.vy = red.speed;
    }
    else {
-     user.vy = 0;           //halt
+     red.vy = 0;           //halt
    }
-}
 
-// Allows user to move   (thank you for the constrain suggestion from E3, Samuel!)
-function userMove() {
-  user.x = constrain(user.x + user.vx, 0, width);
-  user.y = constrain(user.y + user.vy, 0, height);
-}
+  // Red's Move
+  red.x = constrain(red.x + red.vx, 0, width);
+  red.y = constrain(red.y + red.vy, 0, height);
 
-function trailPosition(){
-  for (let i = 0; i < user.trail.length; i++) {
-   let position = user.trail[i];
-   rect(position.x, position.y, user.size);
-  }
-
+  // Red's display
   fill(255,0,0);
-  rect(user.x, user.y, user.size);
-
- let newTrailPosition = {
-   x: user.x,
-   y: user.y,
-  };
-
- user.trail.push(newTrailPosition);
-
- if (user.trail.length > user.trailSize){
-   user.trail.shift();
- }
-}
-
-function moveFood(food){
-  let swap = random(0,1);
-  if (swap < 0.05) {
-    food.vx = random(-food.speed,food.speed);
-    fish.vy = random(-food.speed, food.speed);
-  }
-
-  food.x = food.x + food.vx;
-  food.y = food.y + food.vy;
-
-  food.x = constrain(food.x, 0, width);
-  food.y = constrain(food.y, 0, height);
-}
-
-
-// When user EATS the food
-function checkFood(food){
-  if (!food.eaten) {
-    let d = dist(user.x, user.y, food.x, food.y);
-    if (d < user.size / 2 + food.size / 2) {
-      food.eaten = true;
-    }
-  }
-}
-
-
-// Draws the user as a square
-function displayUser() {
-  push();
-  fill(255,0,0);
-  rect(user.x, user.y, user.size);
-  pop();
-}
-
-function displayFood(food){
-  if (!food.eaten){
-    push();
-    fill(0,0,255);
-    ellipse(food.x, food.y, food.size);
-    pop();
-  }
+  ellipse(red.x,red.y,red.size);
 }
