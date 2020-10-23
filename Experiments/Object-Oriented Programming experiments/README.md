44dc4be4a6e52773e77ebae413d93dc750020a54
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-23 15:28:25 -0400

    Starting to experiment with video 8.1: Object-Oriented Programming

---
+++ Experiments/Object-Oriented Programming experiments/README.md
@@ -0,0 +1,5 @@
+# Object-Oriented Programming
+
+Melissa Banoen-Garde
+CART 253 - B
+Week 8
---
+++ Experiments/Object-Oriented Programming experiments/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**************************************************
+WEEK 8: OBJECT-ORIENTED PROGRAMMING
+Melissa Banoen-Garde
+
+Experimenting with object-oriented programming.
+**************************************************/
+
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+
+function draw() {
+  background(0);
+}
