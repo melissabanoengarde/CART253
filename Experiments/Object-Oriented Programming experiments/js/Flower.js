51716b5889659352507194729b8b5bc40df276de
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-23 17:34:03 -0400

    Learned about adding parameters in OOP. Now starting video 8.3: Object-Oriented Programming and p5 events

---
+++ Experiments/Object-Oriented Programming experiments/js/Flower.js
@@ -0,0 +1,56 @@
+class Flower {
+
+  constructor(x, y, size, stemLength, petalColor) {
+    // Position and size information
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.stemLength = stemLength;
+    this.stemThickness = 10;
+    this.petalThickness = 10;
+    // Color information
+    this.stemColor = {
+      r: 50,
+      g: 150,
+      b: 50
+    };
+    this.petalColor = petalColor;
+    this.centreColor = {
+      r: 50,
+      g: 0,
+      b: 0
+    };
+  }
+
+  //displays flowers
+  display() {      //another method name, replaced "flower" with "this"
+    push();
+    // Draw a line for the stem
+    strokeWeight(this.stemThickness);
+    stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+    line(this.x, this.y, this.x, this.y + this.stemLength);
+    // Draw a circle with a heavy outline for the flower
+    strokeWeight(this.petalThickness);
+    fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+    stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+
+}
+
+
+
+
+/*
+NOTES:
+
+
+methods = functions that exist inside a class
+
+constructor(){} = always in a class, constructs / constructor method
+
+don't need to declare new variable
+
+how to make a flower AND how to display a flower
+*/
