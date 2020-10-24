f9e6e92890be684036efb25f07a5cd30b612a79b
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-24 18:08:50 -0400

    E5: Starting Exercise 05 - Juggle Garden

---
+++ Exercises/Exercise5/README.md
@@ -0,0 +1,5 @@
+# Exercise 05: Juggle Garden
+
+Melissa Banoen-Garde
+Week 8: Object-Oriented Programming
+CART-253 B
---
+++ Exercises/Exercise5/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**************************************************
+Exercise 05: Juggle Garden
+Melissa Banoen-Garde
+
+1 - add a form of user control
+2 - add new class and objects
+3- add at least two endings
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+ createCanvas(800,800);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(250, 255, 176);
+
+}
