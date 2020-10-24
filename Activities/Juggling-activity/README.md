ee1554e43677702974d53b5a18b7b4797bbdedc3
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-23 22:18:10 -0400

    Juggling-activity: Defined a Paddle class, added its move and display method.

---
+++ Activities/Juggling-activity/README.md
@@ -0,0 +1,5 @@
+# Object-Oriented Programming activity
+
+Melissa Banoen-Garde
+CART 253 - B
+Week 7: Object-Oriented Programming 
---
+++ Activities/Juggling-activity/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**************************************************
+Activity: Object-Oriented Programming activity
+Melissa Banoen-Garde
+
+The objective is to get comfortable with defining classes,
+work with interacting classes via collision, and learn to feel
+okay about acceleration and GRAVITY by programming a juggling
+simulation. 
+**************************************************/
+
+
+// Description of setup() goes here.
+function setup() {
+
+}
+
+
+// Description of draw() goes here.
+function draw() {
+
+}
