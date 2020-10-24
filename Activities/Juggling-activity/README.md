b5b85c40850563161b62c7cca74608600b79a0a4
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-23 23:16:03 -0400

    Juggling Activity: Created a new Paddle object and stored it in "paddle". Also called methods "move" and "display" under draw. I will now define a Ball class.

--- Activities/Juggling-activity/README.md
+++ Activities/Juggling-activity/README.md
@@ -1,3 +1,5 @@
-# Template p5 project
+# JUGGLING ACTIVITY
 
-This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+Melissa Banoen-Garde
+Week 8: Object-Oriented Programming
+CART 253 - B
--- Activities/Juggling-activity/js/script.js
+++ Activities/Juggling-activity/js/script.js
@@ -1,22 +1,36 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Juggling Activity
+Melissa Banoen-Garde
 
-Here is a description of this template p5 project.
+The objective of this activty is to get comfortable with
+defining classes, work with interacting classes via collision,
+and learn to feel okay about acceleration and GRAVITY by
+programming a juggling simulation.
 **************************************************/
+
+// Declaring paddle variable
+let paddle;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  // creating new Paddle object
+  paddle = new Paddle(300, 20);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(219, 255, 181);
+
+  // calling Paddle.js's methods
+  paddle.move();
+  paddle.display();
 
 }
