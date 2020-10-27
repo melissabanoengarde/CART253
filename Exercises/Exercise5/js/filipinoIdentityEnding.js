ac767a52301cea8f6fc7d2ac2d1780a20148bbd9
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-27 18:50:44 -0400

    E5: Hoping this will update since push has not updated the changes I did here (i just capitalized the 'f' in filipino)

--- Exercises/Exercise5/js/filipinoIdentityEnding.js
+++ Exercises/Exercise5/js/filipinoIdentityEnding.js
@@ -7,7 +7,7 @@ class FilipinoIdentityEnding extends State {
 
   draw() {
     super.draw();
-    this.display(); 
+    this.display();
   }
 
   // What's displayed if this were the ending
@@ -17,7 +17,7 @@ class FilipinoIdentityEnding extends State {
     textAlign(CENTER, CENTER);
     textSize(40);
     text(this.textString, width/2, height /2);
-    pop();
+    pop(); 
   }
 
 }
