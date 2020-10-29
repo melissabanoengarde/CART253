7d4adae7d5c9efef77e93066c370bdf02405611e
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-29 15:09:53 -0400

    P2-1: Starting Project 2 (part 1)
    Testing and playing with ideas, elements, concepts I had in mind.

---
+++ Projects/Project2/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**************************************************
+PROJECT 2
+Melissa Banoen-Garde
+
+Here is a description of this template p5 project.
+**************************************************/
+
+
+// Description of setup() goes here.
+function setup() {
+
+}
+
+
+// Description of draw() goes here.
+function draw() {
+
+}
