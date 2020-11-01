d58f3e811dfb015630638e291fe0a59c3cd40cb2
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-10-31 22:52:57 -0400

    P2-1: figuring out how motion/movement works in 3d canvas

--- Projects/Project2/js/script.js
+++ Projects/Project2/js/script.js
@@ -4,125 +4,57 @@
 PROJECT 2
 Melissa Banoen-Garde
 
-Here is a description of this template p5 project.
+sketching a solar system simulation with attempt to
+draw with accuracy
 **************************************************/
 
-let myFont;
-let myFont2;
+//Texture images of respective planets
+let neptuneImg;
+let earthImg;
 
-let fullpic;
-
-
-let state = `title`;
-
-// Preloads
+// A preload of all elements in the simulation (img, sound, font)
 function preload() {
-  myFont = loadFont('assets/fonts/Baybayin_Mod.ttf');
-  myFont2 = loadFont('assets/fonts/Leiko-Regular.otf');
-  fullpic = loadImage('assets/images/fullpic.jpg');
-
+  neptuneImg = loadImage('assets/images/neptune.jpg');
+  earthImg = loadImage('assets/images/earth.jpg');
 }
 
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
 }
 
 
 // Description of draw() goes here.
 function draw() {
-  background(0);
-
-  if (state === `title`) {
-    title();
-  } else if (state === `simulation`) {
-    simulation();
-  } else if (state === `lose`) { //lose
-    lose();
-  } else if (state === `win`) { //win
-    win();
-  }
-}
-
-  // orbitControl(5,5,5);
-  // rotateX(frameCount * 0.01);
-  // rotateY(frameCount * 0.01);
-
-function mousePressed() {
-  if (state === `title`) {
-    state = `simulation`
-  }
-}
-
-function title() {
-/* test text */
-  push();
-  rotateY(frameCount * 0.01);
-  fill(0, 255, 17);
-  textFont(myFont);
-  textSize(240);
-  textAlign(CENTER, CENTER);
-  text(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. b`, -200, -1000, 4000,2000);
-  pop();
-
-  push();
-  rotateX(frameCount * -0.01);
-  rotateY(frameCount * -0.01);
-  fill(0, 255,17, 60);
-  textFont(myFont);
-  textSize(180);
-  textAlign(CENTER, CENTER);
-  text(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.b`, -1000, 0, 3000,3000);
-  pop();
-
-  // push();
-  // rotateX(frameCount * 0.0001);
-  // fill(255,255,255);
-  // textFont(myFont2);
-  // textSize(100);
-  // textAlign(CENTER, CENTER);
-  // text(`What is calling you?`, 10,10);
-  // pop();
-
-  push();
-  rotateX(frameCount * 0.0001);
-  fill(255,255,255);
-  textFont(myFont2);
-  textSize(50);
-  textAlign(CENTER, CENTER);
-  text(`CLICK TO BEGIN`, 10,250);
-  pop();
-}
-
-function simulation() {
-
-  // sphere
-  push();
-  stroke(255);
-  noFill();
-  rotateY(frameCount * 0.01);
-  rotateZ(frameCount * 0.005);
-  sphere(300,20,10);
-  pop();
-
-  // cube
-  push();
-  stroke(255);
-  noFill();
-  rotateX(frameCount * 0.01);
-  rotateZ(frameCount * 0.005);
-  box(100,100,100);
-  pop();
-
-  // interactive environment
-  push();
+  background(20);
   noStroke();
-  // rotateY(mouseX * 0.003);
-  rotateY(frameCount * 0.003);
-  texture(fullpic);
-  sphere(width/2,100,300);
-  pop();
+  orbitControl(); // allows user to drag the screen
+
+// neptune
+rotateY(frameCount/2);
+translate(0,0, 0);
+sphere(100,100,100);
+texture(neptuneImg);
+
+// earth
+translate(400, 0, 0);
+rotateY(frameCount * 6);
+sphere(70,70,70);
+texture(earthImg);
+
+
+
+
+
+
+
 }
+
+
+
+
+
 
 
 
@@ -145,5 +77,7 @@ to set light, think of the world's coordinates
 source: https://p5js.org/reference/#/p5/pointLight
 
 rainforest img source: https://i.redd.it/afjau4hzzsv51.jpg
+
+FPS TO DPS: 1 Frames Per Second to Degrees Per Second = 360
 
 */
