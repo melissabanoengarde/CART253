db983db368411f496acf34d435af3aff55f7c28d
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 13:47:40 -0500

    About to make a hearing test

--- Experiments/p5.sound-experiments/js/script.js
+++ Experiments/p5.sound-experiments/js/script.js
@@ -15,13 +15,21 @@ function setup() {
   userStartAudio();
 
     // SYNTAX: new p5.Oscillator([freq], [type]), 440Hz is the default frequency & sine wave is usually the default
-  oscillator = new p5.Oscillator(20, `triangle`);
+  oscillator = new p5.Oscillator(440, `sine`);
 }
 
 
 // Description of draw() goes here.
 function draw() {
   background(0);
+
+  // Frenquency
+  let newFreq = map(mouseY, height, 0, 0, 880);
+  oscillator.freq(newFreq);
+
+  // Amplitude
+  let newAmp = map(mouseX, 0, width, 0, 1);
+  oscillator.amp(newAmp);
 }
 
 
