576f24e821e0d9e73d00e6c5619b179078da6a6d
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 13:55:00 -0500

    hearing test with frequency text. now about to animate the frequency without user input

--- Experiments/p5.sound-experiments/js/script.js
+++ Experiments/p5.sound-experiments/js/script.js
@@ -16,6 +16,9 @@ function setup() {
 
     // SYNTAX: new p5.Oscillator([freq], [type]), 440Hz is the default frequency & sine wave is usually the default
   oscillator = new p5.Oscillator(440, `sine`);
+
+  // Amplitude
+  oscillator.amp(0.1);
 }
 
 
@@ -24,12 +27,15 @@ function draw() {
   background(0);
 
   // Frenquency
-  let newFreq = map(mouseY, height, 0, 0, 880);
+  let newFreq = map(mouseY, height, 0, 20, 20000);   // (20-20000 range of hearing frequency)
   oscillator.freq(newFreq);
 
-  // Amplitude
-  let newAmp = map(mouseX, 0, width, 0, 1);
-  oscillator.amp(newAmp);
+  push();
+  textSize(32);
+  textAlign(LEFT, CENTER);
+  fill(255);
+  text(newFreq, 100, height/2) // displays current frequency 
+  pop();
 }
 
 
