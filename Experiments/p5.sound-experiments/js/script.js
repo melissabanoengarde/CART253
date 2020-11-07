fb681ed1871f037438b3678146b085e102e1243e
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 15:07:01 -0500

    Starting p5.sound 04- p5.AudioIn
    p5.polySynth was really fun to follow through and experiment with :)
    
    I often paused the video to experiment with other values.

--- Experiments/p5.sound-experiments/js/script.js
+++ Experiments/p5.sound-experiments/js/script.js
@@ -7,16 +7,12 @@ Melissa Banoen-Garde
 Experimenting with p5.sound
 **************************************************/
 
-let synth;
-// Notes array which is our scale
-let notes = [`F2`, `G2`, `F2`, `C3`, `C3`, `F2`, `Eb3`, `C3`]; // F-minor scale
-let currentNote = 0;
+
 
 // Description of setup() goes here.
 function setup() {
   createCanvas(600,600);
 
-  synth = new p5.PolySynth();
 
   userStartAudio();
 }
@@ -27,25 +23,6 @@ function draw() {
   background(0);
 }
 
-function keyPressed() {
- // allows you to call a number every milli seconds
- setInterval(playRandomNote, 150);  // SYNTAX: setInterval(function, milliseconds)
-          // ^ you only need to tell it the name of the function to call
-}
-
-
-function playRandomNote() {
-  let note = notes[currentNote];
-  synth.play(note, 1, 0, 0.1);
-     // play([note], [velocity], [secondsFromNow], [sustainTime])
-
-  currentNote = currentNote + 1;
-
-  // this RESTARTS the scale to the bottom again
-  if (currentNote === notes.length) {
-    currentNote = 0;
-  }
-}
 
 
 
@@ -53,8 +30,5 @@ function playRandomNote() {
 /*
 SYNTAX:
 
-new p5.PolySynth([synthVoice], [maxVoices])
-
-play([note], [velocity], [secondsFromNow], [sustainTime])
 
 */
