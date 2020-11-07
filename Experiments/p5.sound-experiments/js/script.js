23c9cda23681b7c2abf16439476a296026d3faf8
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 13:13:24 -0500

    Starting p5.sound 02 - p5.Oscillator

--- Experiments/p5.sound-experiments/js/script.js
+++ Experiments/p5.sound-experiments/js/script.js
@@ -9,11 +9,9 @@ Experimenting with p5.sound
 
 let barkSFX;
 
-// to display "BARK!" or not at the beginning
-let showBarkText = false;
-
 function preload() {
   barkSFX = loadSound(`assets/sounds/bark.wav`);
+
 }
 
 
@@ -22,13 +20,6 @@ function setup() {
   createCanvas(600,600);
   userStartAudio();
 
-  // SYNTAX: addCue(time, callback, [value])
-  // add cues to sound (in seconds) which will either showBark() or hideBark()
-  // in order to only show the text during barking sounds:
-  barkSFX.addCue(0.1, showBark);  // *show*Bark
-  barkSFX.addCue(0.3, hideBark);  // *hide*Bark
-  barkSFX.addCue(0.4, showBark);  // *show*Bark
-  barkSFX.addCue(0.7, hideBark);  // *hide*Bark
 }
 
 
@@ -36,32 +27,13 @@ function setup() {
 function draw() {
   background(0);
 
-  // If showBarkText is true, we display BARK! on canvas
-  if (showBarkText) {
-    push();
-    fill(255);
-    textSize(64);
-    textAlign(CENTER, CENTER);
-    text(`BARK!`, width /2, height/2);
-    pop();
-  }
 }
 
-// Called when the appropriate cue is triggered
-function showBark() {
-  showBarkText = true;
-}
-
-// Called when the appropriate cue is triggered
-function hideBark() {
-  showBarkText = false;
-}
 
 function mousePressed() {
-  barkSFX.play();
+
 }
 
-// mousePressed > showBark function = addCue(0.1, showBark) > hideBark function = addCue(0.3, hideBark)
 
 
 
