64c0ba8934ab568200201e7da622db2625f21f98
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 19:37:21 -0500

    Sound Activity: defined an array of balls in script.js, created a new ball object calling the Ball.js class and its methods, added a for-loop that goes through the balls array, created a function that facilitates the mouse's x and y values to pass onto the Ball's x and y parameters

--- Activities/Sound-activity/js/script.js
+++ Activities/Sound-activity/js/script.js
@@ -8,16 +8,39 @@ Using oscillators to add sound to movement and using a
 synth to add sound to collision.
 **************************************************/
 
+// The balls
+let balls = [];
+
 // setup()
 //
-// Description of setup() goes here.
+// Creates the canvas
 function setup() {
-
+ createCanvas(800, 800);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(20);
 
+  // for-loop that goes through the balls array, calling Ball.js's methods for each one
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX, mouseY);  // passes the values of current mouse position to creatBall (x, y)
+}
+
+
+function createBall(x, y) {
+  // creating new ball object
+  let ball = new Ball(x ,y);
+  // adding ball in balls array
+  balls.push(ball);
 }
