51da4d8d6d5e550ca22e791c38f345b221cd5770
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 20:14:51 -0500

    E6: Started exercise 6

---
+++ Exercises/Exercise6/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Exercise 06: Make Some Noise
+Melissa Banoen-Garde
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
