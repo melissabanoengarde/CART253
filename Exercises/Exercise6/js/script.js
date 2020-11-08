9e898330d1891b7d1dd22d4984e8e429444eece1
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 20:17:58 -0500

    E6: Will continue this after I refine my prototype's code which will simplify how i want to incorporate p5.sound and will serve as additional code to implement in my final project's program folder

--- Exercises/Exercise6/js/script.js
+++ Exercises/Exercise6/js/script.js
@@ -4,7 +4,7 @@
 Exercise 06: Make Some Noise
 Melissa Banoen-Garde
 
-Here is a description of this template p5 project.
+Working with p5.sound and progressing the final project.
 **************************************************/
 
 // setup()
