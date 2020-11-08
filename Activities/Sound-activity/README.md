5404ef93e40be646b3f5d1ba951a786802cfa20f
Melissa Banoen-Garde <m.banoengarde@users.noreply.github.com>
2020-11-07 19:11:28 -0500

    Sound Activity: Starting activity

---
+++ Activities/Sound-activity/README.md
@@ -0,0 +1,5 @@
+# Sound Activity
+
+- Week 10
+- CART 253 - B
+- Melissa Banoen-Garde
---
+++ Activities/Sound-activity/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Sound Activity
+Melissa Banoen-Garde
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
